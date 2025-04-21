dc3fa4560358c5b8a99f6aa32892e1845e908b39
Sachin kushwaha <sachink7755555@gmail.com>
2025-04-21 12:43:15 +0530

    streamlit secrets

--- db.py
+++ db.py
@@ -4,8 +4,10 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import streamlit as st
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+# MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = st.secrets["MONGO_URI"] if "MONGO_URI" in st.secrets else os.getenv("MONGO_URI", "mongodb://localhost:27017")
 
 client = MongoClient(MONGO_URI)
 
